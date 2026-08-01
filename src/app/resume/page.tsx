3c2c6524246a46bdd37b774a55d9b5378131496b
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-01 12:56:17 -0500

    feat: add Resume route shell

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,5 @@
+import { PhaseNotice } from "@/components/site/PhaseNotice";
+
+export default function ResumePage() {
+  return <PhaseNotice eyebrow="DOCUMENTS / TWO PATHS" title="Résumés" description="The final route will provide equal, direct access to the Computational Biology / Bioinformatics and Software Engineering PDF résumés." />;
+}
