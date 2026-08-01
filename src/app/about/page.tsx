0f55d7793e92f046f5b7c160886f25130da18cc3
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-01 12:56:12 -0500

    feat: add About route shell

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import { PhaseNotice } from "@/components/site/PhaseNotice";
+
+export default function AboutPage() {
+  return <PhaseNotice eyebrow="PERSONNEL FILE" title="About" description="The final page will connect software, biology, games, writing, and community without duplicating the résumé." />;
+}
