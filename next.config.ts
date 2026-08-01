68553b218159eaafb5baf68a7ae9b1529b70f1ce
glitchmatrix-jpg <extraextra335@gmail.com>
2026-08-01 12:53:38 -0500

    chore: configure Next.js and MDX

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+import createMDX from "@next/mdx";
+import type { NextConfig } from "next";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
+
+const nextConfig: NextConfig = {
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    contentDispositionType: "attachment",
+  },
+  experimental: {
+    typedRoutes: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" }
+        ],
+      },
+    ];
+  },
+};
+
+export default withMDX(nextConfig);
